5e2d0babc3206bf8901bf18f36009db08450334e
Andrew Hyndman <ajhyndman@hotmail.com>
2020-07-23 03:18:18 -0700

    Draft "applyChangeset" function and tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,13 +1,102 @@
-import { hello } from './index';
+import diff from 'changeset';
+import { Array, Doc } from 'yjs';
 
-describe('test case', () => {
-  it('passes', () => {
-    expect('hello').toBe('hello');
+import { applyChangeset } from './index';
+
+describe('applyChangeset', () => {
+  it('adds keys to map and list', () => {
+    const doc = new Doc();
+    const map = doc.getMap();
+    const array1 = new Array();
+    array1.insert(0, ['abc']);
+    map.set('array-one', array1);
+    map.set('text', 'abc');
+
+    const obj1 = map.toJSON();
+    const obj2 = {
+      ...obj1,
+      'array-one': ['abc', 'def'],
+      extra: true,
+    };
+
+    const changeset = diff(obj1, obj2);
+
+    doc.transact(() => {
+      applyChangeset(map, changeset);
+    });
+
+    expect(map.toJSON()).toEqual(obj2);
   });
-});
 
-describe('hello', () => {
-  it('returns world', () => {
-    expect(hello()).toBe('world');
+  it('constructs new (modifiable) subtrees', () => {
+    const doc = new Doc();
+    const map = doc.getMap();
+
+    const obj1 = map.toJSON();
+    const obj2 = {
+      text: 'abc',
+      'array-one': ['abc'],
+      extra: { foo: true },
+    };
+
+    const changeset = diff(obj1, obj2);
+
+    doc.transact(() => {
+      applyChangeset(map, changeset);
+    });
+
+    expect(map.toJSON()).toEqual(obj2);
+
+    const obj3 = map.toJSON();
+    const obj4 = {
+      ...obj3,
+      'array-one': ['abc', 'def'],
+      extra: { foo: true, bar: false },
+    };
+
+    const changeset2 = diff(obj3, obj4);
+
+    doc.transact(() => {
+      applyChangeset(map, changeset2);
+    });
+
+    expect(map.toJSON()).toEqual(obj4);
+  });
+
+  it('modifies text', () => {
+    const doc = new Doc();
+    const map = doc.getMap();
+    map.set('text', 'abc');
+
+    const obj1 = map.toJSON();
+    const obj2 = {
+      ...obj1,
+      text: 'abcdef',
+    };
+
+    const changeset = diff(obj1, obj2);
+
+    doc.transact(() => {
+      applyChangeset(map, changeset);
+    });
+
+    expect(map.toJSON()).toEqual(obj2);
+  });
+
+  it('deletes keys', () => {
+    const doc = new Doc();
+    const map = doc.getMap();
+    map.set('text', 'abc');
+
+    const obj1 = map.toJSON();
+    const obj2 = {};
+
+    const changeset = diff(obj1, obj2);
+
+    doc.transact(() => {
+      applyChangeset(map, changeset);
+    });
+
+    expect(map.toJSON()).toEqual(obj2);
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,84 @@
-export const hello = () => 'world';
+import { Array as YArray, Map as YMap, Text as YText } from 'yjs';
+
+type YContainer = YArray<any> | YMap<any>;
+
+type Path = string[];
+
+type Change =
+  | {
+      type: 'put';
+      key: Path;
+      value: string | number | boolean;
+    }
+  | {
+      type: 'del';
+      key: Path;
+      value: undefined;
+    };
+
+type ChangeSet = Array<Change>;
+
+const getPath = (container: YContainer, path: Path) => {
+  let currentContainer = container;
+  path.forEach((key) => {
+    if (currentContainer instanceof YArray) {
+      container = currentContainer.get(parseInt(key));
+    } else {
+      container = currentContainer.get(key);
+    }
+  });
+  return container;
+};
+
+const fromJSON = (json: unknown) => {
+  if (json == null || ['boolean', 'number'].includes(typeof json)) {
+    return json;
+  }
+  if (typeof json === 'string') {
+    const text = new YText();
+    text.insert(0, json);
+    return text;
+  }
+  if (Array.isArray(json)) {
+    const elements = json.map(fromJSON);
+    const array = new YArray();
+    array.insert(0, elements);
+    return array;
+  }
+  if (Object.keys(json).length > 0) {
+    const values = Object.values(json).map(fromJSON);
+    const map = new YMap();
+    Object.keys(json).forEach((key, i) => {
+      map.set(key, values[i]);
+    });
+    return map;
+  }
+};
+
+export const applyChangeset = (container: YContainer, changeset: ChangeSet) => {
+  console.log(changeset);
+  changeset.forEach(({ type, key: path, value }) => {
+    // console.log(type, path, value);
+    const lastKey = path[path.length - 1];
+    const containerToUpdate = getPath(container, path.slice(0, -1));
+    const yValue = fromJSON(value);
+    switch (type) {
+      case 'put':
+        if (containerToUpdate instanceof YArray) {
+          containerToUpdate.insert(parseInt(lastKey), [yValue]);
+        } else {
+          containerToUpdate.set(lastKey, yValue);
+        }
+        break;
+      case 'del':
+        if (containerToUpdate instanceof YArray) {
+          containerToUpdate.delete(parseInt(lastKey));
+        } else {
+          containerToUpdate.delete(lastKey);
+        }
+        break;
+      default:
+        throw new Error();
+    }
+  });
+};
